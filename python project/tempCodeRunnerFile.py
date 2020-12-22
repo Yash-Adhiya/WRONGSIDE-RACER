6ef902f8626499be8b1da42dfb4dd009eed19f24
Yash-Adhiya <76459878+Yash-Adhiya@users.noreply.github.com>
2020-12-22 07:49:00 +0530

    Add files via upload

---
+++ python project/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+def background():
+	display.blit(backgroundleft,(0,0))	#set left side position of background image at x axis & y axis 
+	display.blit(backgroundright,(700,0))	#set right side position of background image at x axis & y axis
